90d0b7efab4becff9fc0b68da849c0c564c9a35b
EngMohamed95 <eng.mhamed99@gmail.com>
2026-06-10 21:00:30 +0300

    feat: create global CSS stylesheet and update file-checking script to monitor CSS directory

--- check.mjs
+++ check.mjs
@@ -14,10 +14,10 @@ async function check() {
   const client = new ftp.Client();
   try {
     await client.access(config);
-    await client.cd('/public_html/static/js');
-    const jsList = await client.list();
-    console.log('--- JS FILES ---');
-    jsList.forEach(f => console.log(' ', f.name, f.size, 'bytes', f.permissions));
+    await client.cd('/public_html/static/css');
+    const cssList = await client.list();
+    console.log('--- CSS FILES ---');
+    cssList.forEach(f => console.log(' ', f.name, f.size, 'bytes', f.permissions));
   } catch (err) {
     console.error('Error:', err.message);
   } finally {
